c8fd2b1f5476e2499db678962b8275ac86843599
Edison Caceres <edisonyca127@gmail.com>
2025-02-19 10:31:06 -0500

    Created a navbar component, unstyled.

---
+++ pages/components/Navbar.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export default function Navbar(){
+    return(
+        <Nav>
+            <Link href="#dasboard">Dashboard</Link>
+            <Link href="#blogs">Blog</Link>
+            <Button>Log In</Button>
+            <Button>Try Money Moves</Button>
+        </Nav>
+    );
+}
+
+const Nav = styled.nav`
+    background-color: #FF0000;
+    padding: 10px;
+    text-align: right;
+`;
+
+const Link = styled.a `
+    margin-right: 20px;
+`;
+
+const Button = styled.button `
+    margin-right: 20px;
+`;
+
+
