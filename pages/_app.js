c388a9514c6a9f392793e0ecbfb592981b5f555b
Edison Caceres Alejo <edisonyca127@gmail.com>
2025-02-19 22:59:49 -0500

    Finished Section 1 (without styles) started section 2.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import {Work_Sans} from 'next/font/google';
+
+const work_sans = Work_Sans({subsets: ['latin']})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <main className={work_sans.className}>
+    <Component {...pageProps} />
+  </main>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,11 @@
-import Navbar from "./components/Navbar";
-import styled from 'styled-components';
-
+import Navbar from "./components/landing/Navbar";
+import Hero from "./components/landing/Hero";
 
 export default function Home() {
   return (
     <>
       <Navbar/>
-      <Main>
-        <Section>
-          <HeadLinerSpan>
-            <EyeCatcherContainer>
-              <HeadLine>Your Money, Your Plan, Your Future.</HeadLine>
-              <HeadLineFollower>Through MoneyMoves you can track your spending, set goals, and make smarter financial decisions.</HeadLineFollower>
-              <GetStartedBtn>Get Started</GetStartedBtn>
-            </EyeCatcherContainer>
-            <EyeCatcherPhoto>
-              <Image src="/temp_money.jpg"/>
-            </EyeCatcherPhoto>
-          </HeadLinerSpan>
-        </Section>
-        <Section>
-            <p> Section 2 </p>
-        </Section>
-        <Section> 
-            <p> Section 3 </p>
-        </Section>
-      </Main>
+      <Hero></Hero>
     </>
   );
 }
-
-const Main = styled.main`
-
-`;
-
-const Section = styled.section`
-  background-color: blue; 
-  height: 650px;
-  position: relative;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-`;
-
-const EyeCatcherContainer = styled.div`
-  position: absolute;
-  left: 5%;
-  top: 10%;
-  height: 80%;
-  width: 400px;
-  padding-left: 15px;
-`;
-
-const EyeCatcherPhoto = styled.div`
-  background-color: black;
-  left: 45%;
-  top: 10%;
-  height: 80%;
-  width: 600px;
-  position: absolute;
-`;
-
-const HeadLine = styled.h1`
-  color: white;
-`;
-
-const HeadLineFollower = styled.p`
-  color: white;
-`;
-
-const GetStartedBtn = styled.button``;
-
-const HeadLinerSpan = styled.span``;
