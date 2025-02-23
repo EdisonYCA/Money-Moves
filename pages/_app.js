927b081a12e3710f12e2e1544e914c23deecf494
Edison Caceres Alejo <edisonyca127@gmail.com>
2025-02-23 18:53:34 -0500

    Got dashboard working with chart, working on adding remaining pieces to front-end

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import {Work_Sans} from 'next/font/google';
+import GlobalStyle from '@/styles/GlobalStyles';
 
 const work_sans = Work_Sans({subsets: ['latin']})
 
 export default function App({ Component, pageProps }) {
   return (
-  <main className={work_sans.className}>
-    <Component {...pageProps} />
-  </main>
+    <main className={work_sans.className}>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </main>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Navbar from "./components/landing/Navbar";
-import Hero from "./components/landing/Hero";
+import Navbar from "@/components/landing/Navbar";
+import Hero from "@/components/landing/Hero";
 
 export default function Home() {
   return (
