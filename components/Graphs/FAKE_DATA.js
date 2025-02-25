ac283075dfba1a4aaa4bd2b789b0451f56033037
Edison Caceres <edisonyca127@gmail.com>
2025-02-25 03:47:45 -0500

    Ready for tomorrows checkpoint.

--- components/Graphs/FAKE_DATA.js
+++ components/Graphs/FAKE_DATA.js
@@ -1,140 +1,85 @@
 export const barChartData = {
-    labels: ["Rent", "Groceries", "Utilities", "Entertainment", "Transportation"],
-    datasets: [
-      {
-        label: "Expenses",
-        data: [1800, 300, 180, 150, 100],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-        ],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.5)",
-          "rgba(54, 162, 235, 0.5)",
-          "rgba(255, 206, 86, 0.5)",
-          "rgba(75, 192, 192, 0.5)",
-          "rgba(153, 102, 255, 0.5)",
-        ],
-        borderWidth: 1,
+  labels: ["Rent", "Groceries", "Utilities", "Entertainment", "Transportation"],
+  datasets: [
+    {
+      label: "January",
+      data: [1800, 300, 180, 150, 100],
+      borderColor: "#000000",
+      backgroundColor: "#000000",
+      borderWidth: 1,
+      barPercentage: 0.4,
+      categoryPercentage: 0.6,
+    },
+    {
+      label: "February",
+      data: [2500, 200, 150, 500, 150],
+      borderColor: "#248232",
+      backgroundColor: "#248232",
+      borderWidth: 1,
+      barPercentage: 0.4,
+      categoryPercentage: 0.6,
+    },
+    {
+      label: "March",
+      data: [2500, 250, 350, 200, 100],
+      borderColor: "#2D3A3A",
+      backgroundColor: "#2D3A3A",
+      borderWidth: 1,
+      barPercentage: 0.4,
+      categoryPercentage: 0.6,
+    },
+  ],
+  options: {
+    indexAxis: "x",
+    plugins: {
+      title: {
+        display: true,
+        text: "Income Distribution",
+        font: {
+          size: 15,
+        },
       },
-    ],
-    options: {
-      plugins: {
-        title: {
-          display: true,
-          text: "Paycheck Distribution",
+      legend: {
+        labels: {
           font: {
-            size: 25,
+            size: 10,
           },
-        },
-        legend: {
-          labels: {
-            font: {
-              size: 25,
-            },
-            boxWidth: 50,
-            boxHeight: 30
-          },
-        },
-        tooltip: {
-          bodyFont: {
-            size: 25,
-          },
-          titleFont: {
-            size: 25,
-          },
-          padding: 25,
+          boxWidth: 10,
+          boxHeight: 10,
         },
       },
-      scales: {
-        x: {
-          ticks: {
-            font: {
-              size: 30,
-            },
-          },
+      tooltip: {
+        callbacks: {
+          label: (tooltipItem) => `$${tooltipItem.raw}`,
         },
-        y: {
-          ticks: {
-            font: {
-              size: 30,
-            },
-          },
+        bodyFont: {
+          size: 10,
         },
+        titleFont: {
+          size: 10,
+        },
+        padding: 10,
       },
     },
-
-    labels: ["Rent", "Groceries", "Utilities", "Entertainment", "Transportation"],
-    datasets: [
-      {
-        label: "Expenses",
-        data: [1800, 300, 180, 150, 100],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-        ],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.5)",
-          "rgba(54, 162, 235, 0.5)",
-          "rgba(255, 206, 86, 0.5)",
-          "rgba(75, 192, 192, 0.5)",
-          "rgba(153, 102, 255, 0.5)",
-        ],
-        borderWidth: 1,
-      },
-      
-    ],
-    options: {
-      indexAxis: "y",
-      plugins: {
-        title: {
-          display: true,
-          text: "Paycheck Distribution",
+    scales: {
+      x: {
+        ticks: {
           font: {
-            size: 25,
-          },
-        },
-        legend: {
-          labels: {
-            font: {
-              size: 25,
-            },
-            boxWidth: 50,
-            boxHeight: 30
-          },
-        },
-        tooltip: {
-          bodyFont: {
-            size: 25,
-          },
-          titleFont: {
-            size: 25,
-          },
-          padding: 25,
-        },
-      },
-      scales: {
-        x: {
-          ticks: {
-            font: {
-              size: 30,
-            },
-          },
-        },
-        y: {
-          ticks: {
-            font: {
-              size: 30,
-            },
+            size: 10,
           },
         },
       },
-    }
-  };
-  
+      y: {
+        ticks: {
+          font: {
+            size: 10,
+          },
+          callback: (value) => {
+            return value >= 1000 ? `$${(value / 1000).toFixed(1)}k` : `$${value}`;
+          },
+        },
+        suggestedMax: 4100,
+      },
+    },
+  },
+};
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,46 @@
-import Navbar from "@/components/landing/Navbar";
+import DashNavBar from "@/components/Dashboard/NavBar";
 import styled from "styled-components";
 import { BarChart } from "@/components/Graphs/Bar";
 export default function Dashboard() {
   return (
     <PageContainer>
-      <Navbar dashboard={true}></Navbar>
+      <NavigationPanel></NavigationPanel>
+
+      <MainContentContainer>
+        <DashNavBar />
+        <MoneyOverview>
+          <ExpenseContainer color="#1d6829">
+            <ExpenseText>Monthly Income</ExpenseText>
+            <ExpenseHeader>$4.1k</ExpenseHeader>
+          </ExpenseContainer>
+          <ExpenseContainer color="#2AA84A">
+            <ExpenseText>Monthly Expenses</ExpenseText>
+            <ExpenseHeader>$2k</ExpenseHeader>
+          </ExpenseContainer>
+          <ExpenseContainer color="#2D3A3A">
+            <ExpenseText>Net Income</ExpenseText>
+            <ExpenseHeader>$1.1k</ExpenseHeader>
+          </ExpenseContainer>
+          <ExpenseContainer>
+            <ExpenseText color="black">Savings Rate</ExpenseText>
+            <ExpenseHeader color="black">3.2%</ExpenseHeader>  
+          </ExpenseContainer>
+        </MoneyOverview>
+        <TransactionsAnalytics>
+          <GraphContainer>
+            <BarChart/>
+          </GraphContainer>
+          <PieContainer>
+            
+          </PieContainer>
+          <TransactionsContainer>
+
+          </TransactionsContainer>
+        </TransactionsAnalytics>
+        <InvestmentsAndBills>
+        </InvestmentsAndBills>
+      </MainContentContainer>
+      {/* <Navbar dashboard={true}></Navbar>
       <PaycheckContainer>
         <ExpenseContainer>
           <ExpenseHeader>$4,0000</ExpenseHeader>
@@ -22,49 +58,127 @@ export default function Dashboard() {
       <BarChartContainer>
         <BarChart/>
       </BarChartContainer>
+*/}
     </PageContainer>
   );
 }
+
 const PageContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: #f0f0f0;
+  display: flex;
 `;
 
-const PaycheckContainer = styled.div`
-  width: 100%;
-  height: 350px;
+const NavigationPanel = styled.div`
+  width: 20%;
+  height: 100%;
+  background-color: #000000;
   display: flex;
+`;
+
+const MainContentContainer = styled.div`
+  width: 80%;
+  height: 100%;
+  background-color: #f0f0f0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MoneyOverview = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
   align-items: center;
-  justify-content: left;
+  gap: 25px;
+  padding-top: 80px;
 `;
 
 const ExpenseContainer = styled.div`
-  width: 500px;
-  height: 200px;
+  width: 210px; /* Increased for better visibility */
+  height: 100px;
+  background-color: ${props => props.color ? props.color : 'white'};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  justify-content: center;
+  padding: 10px;
+  border-radius: 16px; 
+
+  &:hover {
+      transform: translateY(-5px);
+      box-shadow: 0px 6px 15px rgba(0, 0, 0, 0.15);
+  }
+`;
+
+const TransactionsAnalytics = styled.div`
+  width: 100%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  padding: 13px;
+`;
+
+const GraphContainer = styled.div`
+  width: 48%;
+  height: 100%;
+  display: flex;
+  background-color: white;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  border-radius: 16px; 
+`;
+
+const PieContainer = styled.div`
+  width: 25%;
+  height: 100%;
+  display: flex;
+  background-color: yellow;
+`;
+
+const TransactionsContainer = styled.div`
+  width: 25%;
+  height: 100%;
+  display: flex;
+  background-color: black;
+`;
+
+const InvestmentsAndBills = styled.div`
+  width: 100%;
+  height: 50%;
+  display: flex;
+  background-color: green;
+`;
+
+const Investments = styled.div`
+  width: 100px;
+  height: 100px;
   background-color: white;
   margin: 0 90px 0 90px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 2px solid black;
 `;
 
-const ExpenseHeader = styled.h1`
-  color: ${props => props.expense ? 'red' : 'green'};
-  font-size: 50px;
-`;
-const ExpenseText = styled.h5`
-  color: grey;
-  font-size: 40px;
-`;
-
-const BarChartContainer = styled.div`
-    width: 49%;
-    height: 2000px;
-    display: flex;
-    background-color: red;
-    margin-left: 90px;
+const Bills = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: white;
+  margin: 0 90px 0 90px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
+
+const ExpenseHeader = styled.p`
+  color: ${props => props.color ? props.color : 'white'};
+  font-size: 2.5rem;
+  margin-top: 20px;
+`;
+
+const ExpenseText = styled.p`
+  color: ${props => props.color ? props.color : 'white'};
+`;
