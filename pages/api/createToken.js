642d22e1b1cc1ed305157f5bd26e1c602f65ed16
Edison Caceres <edisonyca127@gmail.com>
2025-03-06 12:22:01 -0500

    adding

--- pages/api/createToken.js
+++ pages/api/createToken.js
@@ -1,16 +1,23 @@
-import {client, PLAID_COUNTRY_CODES, PLAID_PRODUCTS} from '@/library/plaid';
+import { client, PLAID_COUNTRY_CODES, PLAID_PRODUCTS } from "@/library/plaid";
 
-export default async function handler(req, res){
-    try{
-        const response = await client.linkTokenCreate({
-            client_name: 'Plaid Test App',
-            user: { client_user_id: 'user-123' },
-            products: PLAID_PRODUCTS,
-            country_codes: PLAID_COUNTRY_CODES,
-            language: 'en',
-        })
-        res.status(200).json(response.data)
-    } catch(err){
-        res.status(500).json({error: err})
-    }
+export default async function handler(req, res) {
+  try {
+    const response = await client.linkTokenCreate(
+      {
+        client_name: "Plaid Test App",
+        user: { client_user_id: "user-123" },
+        products: PLAID_PRODUCTS,
+        country_codes: PLAID_COUNTRY_CODES,
+        language: "en",
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.status(200).json(response.data);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 }
