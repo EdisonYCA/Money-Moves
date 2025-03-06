2f79ea6cc7d9d7d67ef0f26c65c515cb7f717a00
Edison Caceres <edisonyca127@gmail.com>
2025-03-05 23:48:21 -0500

    metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
 import { Work_Sans } from "next/font/google";
 import "@/styles/globals.css";
 import { StateContext } from "@/context/StateContext";
+import Head from "next/head";
 
 const work_sans = Work_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <StateContext>
-      <main className={`${work_sans.className}`}>
-        <Component {...pageProps} />
-      </main>
-    </StateContext>
+    <>
+      <Head>
+          <title>MoneyMoves</title>
+        </Head>
+      <StateContext>
+        <main className={`${work_sans.className}`}>
+          <Component {...pageProps} />
+        </main>
+      </StateContext>
+    </>
   );
 }
