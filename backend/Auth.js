611a57cb0862d92f6ffd0cdb8c1d555d8aa20f98
Edison Caceres Alejo <edisonyca127@gmail.com>
2025-03-06 20:03:57 -0500

    make sure users from google sign up get intitaized in db

--- backend/Auth.js
+++ backend/Auth.js
@@ -1,46 +1,56 @@
-import { auth } from '@/library/firebaseConfig.js'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
-import { initFirestoreNewUser } from './Database';
+import { auth } from "@/library/firebaseConfig.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { initFirestoreNewUser } from "./Database";
 
 const provider = new GoogleAuthProvider();
 
 export const registerUser = (email, password, setUser) => {
-    createUserWithEmailAndPassword(auth, email, password)
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-        const user = userCredential.user;
-        setUser(user);
-        initFirestoreNewUser(user.uid)
+      const user = userCredential.user;
+      setUser(user);
+      initFirestoreNewUser(user.uid);
     })
     .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+      const errorCode = error.code;
+      const errorMessage = error.message;
     });
-}
+};
 
 export const logUserIn = (email, password, setUser) => {
-    signInWithEmailAndPassword(auth, email, password)
+  signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-        const user = userCredential.user;
-        setUser(user)
+      const user = userCredential.user;
+      setUser(user);
     })
     .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+      const errorCode = error.code;
+      const errorMessage = error.message;
     });
-}
+};
 
 export const logUserInGoogle = (setUser) => {
-    signInWithPopup(auth, provider)
+  signInWithPopup(auth, provider)
     .then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        const user = result.user;
-        setUser(user);
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.customData.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      setUser(user);
+      const isNewUser = result.additionalUserInfo?.isNewUser;
+      if (isNewUser) {
+        initFirestoreNewUser(user.uid);
+      }
+      initFirestoreNewUser(user.uid);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
     });
-}
-
+};
